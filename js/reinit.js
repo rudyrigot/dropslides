bfc06622ef12fe2c5371dc277b13bf2a9070cdf3
Rudy Rigot <rudy.rigot@gmail.com>
2014-03-22 18:06:41 -0700

    Moving the reinit structure in the HTML

--- js/reinit.js
+++ js/reinit.js
@@ -25,45 +25,6 @@ $(function(){
         slide.deleteRecord();
       });
 
-      var newRecords = [
-        {
-          title: '30 minutes to build a useful productivity app',
-          body: '&hellip; with the awesome Dropbox Datastore API!',
-          position: '1000',
-          disabled: false
-        },
-        {
-          title: 'What you know of Dropbox',
-          body: '<img src="img/syncbox_transparent.png" style="background-color: white">',
-          position: '2000',
-          disabled: false
-        },
-        {
-          title: 'What you should know of Dropbox',
-          body: 'aka the Datastore API<br><img src="img/developer-datastores-vfliUo8_8.png" style="background-color: white">',
-          position: '3000',
-          disabled: false
-        },
-        {
-          title: 'Step 1 - the idea',
-          body: '',
-          position: '4000',
-          disabled: false
-        },
-        {
-          title: 'Step 1 - the idea',
-          body: 'What if we made a slide composer today?',
-          position: '5000',
-          disabled: false
-        },
-        {
-          title: 'Step 2 - Hot damn! I didn\'t make the other slides!!',
-          body: 'Well, let\'s <a href="index.html">work this out</a> together&hellip;',
-          position: '6000',
-          disabled: false
-        }
-      ];
-
       _.each(newRecords, function(element){
         slidesTable.insert(element);
       });
